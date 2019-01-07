71a62d0e300157177076f99553f138c7b510ca83
sasedev <seif.salah@gmail.com>
2019-01-07 15:33:06 +0100

    Fix deprecation on symfony/config 4.2

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,9 +15,11 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        //$rootNode = $treeBuilder->root('sasedev_twig_extensions');
-        $treeBuilder->root('sasedev_twig_extensions');
+        $treeBuilder = new TreeBuilder('sasedev_twig_extensions');
+        /**
+         * @see https://github.com/symfony/monolog-bundle/pull/281/commits/45742f1a97291edd1401ecb58024488bbd793038
+         */
+        //$rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('sasedev_twig_extensions');
 
         return $treeBuilder;
     }
